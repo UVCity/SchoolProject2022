0e4a6a73bd9d6c36eb765cb8a67285387e46a610
DkirkPorter <steve667a998@gmail.com>
2022-03-23 23:44:42 -0400

    Import JavaFX imports Instantiate objects for data entry stage

--- src/main/java/edu/bsu/cs222/LetsLynkApplication.java
+++ src/main/java/edu/bsu/cs222/LetsLynkApplication.java
@@ -1,9 +1,35 @@
 package edu.bsu.cs222;
 
 
-public class LetsLynkApplication {
+import javafx.application.Application;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+
+public class LetsLynkApplication extends Application {
+
+    private final TextField address1 = new TextField("Street address 1");
+    private final TextField city1 = new TextField("City name 1");
+    private final TextField zip1 = new TextField("Zipcode 1");
+
+    private final TextField address2 = new TextField("Street address 2");
+    private final TextField city2 = new TextField("City name 2");
+    private final TextField zip2 = new TextField("Zipcode 2");
+
+    private final Button goButton = new Button("Go!");
+
+    @Override
+    public void start(Stage dataEnterStage) {
 
     }
 }
