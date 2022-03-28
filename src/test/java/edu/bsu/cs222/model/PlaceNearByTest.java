9dbc1bf20aea6c66e500ee5928c2f7c9fa14d9a1
UVCITY <ld77668952@gmail.com>
2022-03-28 16:28:45 -0400

    Create New test file for DistanceTest Refactor LetsLynkTests to PlaceFromTextTest

--- src/test/java/edu/bsu/cs222/model/PlaceNearByTest.java
+++ src/test/java/edu/bsu/cs222/model/PlaceNearByTest.java
@@ -12,30 +12,27 @@ public class PlaceNearByTest {
     public final InputStream placeNearByStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("PlaceNearBySearchData.json");
     public final AddressParser parser = new AddressParser();
     @Test
-    public void ParseVenueAddressTest() throws IOException {
+    public void parseVenueAddressTest() throws IOException {
         String address = parser.parseVenueAddress(placeNearByStream);
         Assertions.assertEquals("523 South Tillotson Avenue, Muncie",address);
     }
-
     @Test
-    public void ParseVenueNameTest() throws IOException {
+    public void parseVenueNameTest() throws IOException {
         String name = parser.parseName(placeNearByStream);
         Assertions.assertEquals("Starbucks",name);
     }
-
     @Test
-    public void ParseVenueHoursOfOperationTest() throws IOException {
+    public void parseVenueHoursOfOperationTest() throws IOException {
         String openValue = parser.parseHoursOfOperation(placeNearByStream);
         Assertions.assertEquals("true", openValue);
     }
-
     @Test
-    public void ParseVenueLongitudeTest() throws IOException {
+    public void parseVenueLongitudeTest() throws IOException {
         String longitude = parser.parseLongitude(placeNearByStream);
         Assertions.assertEquals("-85.419575",longitude);
     }
     @Test
-    public void ParseVenueLatitudeTest() throws IOException {
+    public void parseVenueLatitudeTest() throws IOException {
         String latitude = parser.parseLatitude(placeNearByStream);
         Assertions.assertEquals("40.189823", latitude);
     }
