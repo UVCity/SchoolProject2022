dd46b6e9cb616698524f96c281d80bf5c3a18386
UVCITY <ld77668952@gmail.com>
2022-04-05 17:51:17 -0400

    Complete Refactor. No Dry code

--- src/main/java/edu/bsu/cs222/AddressParser.java
+++ src/main/java/edu/bsu/cs222/AddressParser.java
@@ -13,35 +13,7 @@ import java.nio.charset.Charset;
 
 public class AddressParser {
 
-    public String key() throws IOException {
-        InputStream Key = Thread.currentThread().getContextClassLoader().getResourceAsStream("./privateResources/Key.Json");
-        JSONArray address = JsonPath.read(Key, "$..key");
-        return address.get(0).toString();
-    }
-
-    public InputStream placeFromText(String address) throws IOException {
-        String encodedUrlString = URLEncoder.encode(address, Charset.defaultCharset());
-
-        String urlString = String.format("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=%s&inputtype=textquery&fields=formatted_address%%2Cgeometry&key=%s",encodedUrlString , key());
-        return tryCatch(urlString);
-    }
-    private InputStream placeNearSearch(Double latitude, Double longitude, String venueType) throws IOException {
-        String format = "%2C";
-        String formatter = String.format("%f%s%f",latitude,format, longitude);
-        String urlString = String.format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s&rankby=distance&type=%s&formatted_address&key=%s",formatter, venueType, key());
-        return tryCatch(urlString);
-    }
-    private InputStream directionsAndDistance( String placeFrom , String placeTo) throws IOException {
-        String from = URLEncoder.encode(placeFrom, Charset.defaultCharset());
-        String to = URLEncoder.encode(placeTo, Charset.defaultCharset());
-        String urlString = String.format("https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&key=%s" ,from, to, key());
-        return tryCatch(urlString);
-    }
-    //______________________________________________________________________________________________________________________
-    //returnAverage will need to be modified to run more than two sets of data and return their average
-    public Double returnAverage(Double lat_long1, Double lat_long2){
-        return (lat_long1 + lat_long2)/2;
-    }
+    //______________________________________________________________________________P A R S E R S
     public String parseVenueAddress(InputStream testDataStream) throws IOException {
         JSONArray address = JsonPath.read(testDataStream, "$..vicinity");
         return address.get(0).toString();
@@ -58,19 +30,51 @@ public class AddressParser {
         JSONArray openValue = JsonPath.read(testDataStream, "$..open_now");
         return openValue.get(0).toString();
     }
-    public Double parseLongitude (InputStream testDataStream) throws IOException {
-        JSONArray longitude = JsonPath.read(testDataStream, "$..location.lng");
-        return (Double) longitude.get(0);
-    }
-    public Double parseLatitude (InputStream testDataStream) throws IOException {
-        JSONArray latitude = JsonPath.read(testDataStream, "$..location.lat");
-        return (Double) latitude.get(0);
-    }
-    public String parseDistanceToAddress(InputStream testDataStream) throws IOException{
+    public String parseDistanceToAddress(InputStream testDataStream) throws IOException {
         JSONArray distance = JsonPath.read(testDataStream, "$..distance.text");
         return distance.get(0).toString();
     }
+    public Double[] parseLatitudeAndLongitude(InputStream testDataStream) throws IOException {
+        JSONArray latitudeAndLongitude = JsonPath.read(testDataStream, "$..location[?(@.lat)]");
+        return splitLatitudeAndLongitude(latitudeAndLongitude.get(0).toString());
+    }
+//________________________________________________________________________________________
+    public InputStream placeFromText(String address) throws IOException {
+        String encodedUrlString = URLEncoder.encode(address, Charset.defaultCharset());
 
+        String urlString = String.format("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=%s&inputtype=textquery&fields=formatted_address%%2Cgeometry&key=%s",encodedUrlString , key());
+        return tryCatch(urlString);
+    }
+    public InputStream placeNearSearch(Double latitude, Double longitude, String venueType) throws IOException {
+        String format = "%2C";
+        String formatter = String.format("%f%s%f",latitude,format, longitude);
+        String urlString = String.format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s&rankby=distance&type=%s&formatted_address&key=%s",formatter, venueType, key());
+        return tryCatch(urlString);
+    }
+    public InputStream directionsAndDistance( String placeFrom , String placeTo) throws IOException {
+        String from = URLEncoder.encode(placeFrom, Charset.defaultCharset());
+        String to = URLEncoder.encode(placeTo, Charset.defaultCharset());
+        String urlString = String.format("https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&key=%s" ,from, to, key());
+        return tryCatch(urlString);
+    }
+    public Double[] returnAverage(Double[] coordinatesA, Double[] coordinatesB) {
+        Double[] result = new Double[2];
+        result[0] = (coordinatesA[0] + coordinatesB[0])/2;
+        result[1] = (coordinatesA[1] + coordinatesB[1])/2;
+        return result;
+    }
+    public Double[] splitLatitudeAndLongitude(String locationData) {
+        String lat = locationData.replace("{lat=", "");       // delete "{lat="
+        String latitude = (lat.split(",")[0]);                // pull string before ","
+
+        String lon = locationData.replace("}", "");           // delete "}"
+        String longitude = (lon.split("lng=")[1]);            // pull string after "lng="
+
+        Double[] coordinates = new Double[2];
+        coordinates[0] = (Double.parseDouble(latitude));
+        coordinates[1] = (Double.parseDouble(longitude));
+        return coordinates;
+    }
     public InputStream tryCatch(String urlString)throws IOException{
         try {
             URL url = new URL(urlString);
@@ -82,5 +86,11 @@ public class AddressParser {
         catch (MalformedURLException malformedURLException){
             throw new RuntimeException(malformedURLException);
         }
+    }
+//_____________________________________________________________________________________________________________________________
+    public String key() throws IOException {
+        InputStream Key = Thread.currentThread().getContextClassLoader().getResourceAsStream("./privateResources/Key.Json");
+        JSONArray address = JsonPath.read(Key, "$..key");
+        return address.get(0).toString();
     }
 }
--- src/test/java/edu/bsu/cs222/model/PlaceFromTextTest.java
+++ src/test/java/edu/bsu/cs222/model/PlaceFromTextTest.java
@@ -2,6 +2,7 @@ package edu.bsu.cs222.model;
 
 import edu.bsu.cs222.AddressParser;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -15,41 +16,12 @@ public class PlaceFromTextTest {
     @Test
     public void parseUserAddressTest() throws IOException {
         String address = parser.parseUserAddress(placeFromTextStream);
-        Assertions.assertEquals("2720 W Jackson St, Muncie, IN 47303, USA",address);
+        Assertions.assertEquals("2720 W Jackson St, Muncie, IN 47303, USA", address);
     }
     @Test
-    public void parseUserLongitude() throws IOException {
-        Double longitude = parser.parseLongitude(placeFromTextStream);
-        Assertions.assertEquals(-85.4186142,longitude);
+    public void parseUserLatitudeAndLongitudeTest() throws IOException {
+        Double[] latitudeAndLongitude = parser.parseLatitudeAndLongitude(placeFromTextStream);
+        Assertions.assertEquals(40.1934735, latitudeAndLongitude[0]);
+        Assertions.assertEquals(-85.4186142, latitudeAndLongitude[1]);
     }
-    @Test
-    public void parseUserLatitudeTest() throws IOException {
-        Double latitude = parser.parseLatitude(placeFromTextStream);
-        Assertions.assertEquals(40.1934735, latitude);
-    }
-    //________________U_R_L___T_E_S_T_I_N_G______________
-    @Test
-    public void parseUsersAverageLatitudeTest() throws IOException {
-        InputStream userOneAddress = parser.placeFromText("1615 Riverside Avenue, 47303"); // Ideally these address would be unique
-        Double latitude1 = parser.parseLatitude(userOneAddress);
-        InputStream userTwoAddress = parser.placeFromText("2800 Tillotson Avenue 47304");
-        Double latitude2 = parser.parseLatitude(userTwoAddress);
-        Double avgLat = parser.returnAverage(latitude1, latitude2);
-        Assertions.assertEquals(40.208118150000004, avgLat);
-    }
-    @Test
-    public void parseUsersAverageLongitudeTest() throws IOException {
-        InputStream userOneAddress = parser.placeFromText("1615 Riverside Avenue, 47303"); // Ideally these address would be unique
-        Double longitude1 = parser.parseLongitude(userOneAddress);
-        InputStream userTwoAddress = parser.placeFromText("2800 Tillotson Avenue 47304");
-        Double longitude2 = parser.parseLongitude(userTwoAddress);
-        Double avgLong = parser.returnAverage(longitude1, longitude2);
-        Assertions.assertEquals(-85.41264945, avgLong);
-    }
-    @Test
-    public void parseUserAddressFromURLTest() throws IOException {
-        String address = parser.parseVenueAddressURL(40.208118150000004, -85.41264945, "restaurant");
-        Assertions.assertEquals("Ball State University North Dining 201, 1525 North McKinley Avenue, Muncie", address);
-    }
-
 }
