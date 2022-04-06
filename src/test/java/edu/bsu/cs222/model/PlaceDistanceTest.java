4c4e69835dd1e586b891be81cf44ce1f9169ec1a
UVCITY <ld77668952@gmail.com>
2022-04-06 16:27:22 -0400

    Push CompleteRefactor Branch to Master

--- src/test/java/edu/bsu/cs222/model/PlaceDistanceTest.java
+++ src/test/java/edu/bsu/cs222/model/PlaceDistanceTest.java
@@ -8,13 +8,17 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class PlaceDistanceTest {
-
     private final InputStream placeDirectionsStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("Directions.json");
     private final AddressParser parser = new AddressParser();
-
     @Test
     public void parseDistance() throws IOException {
         String distance = parser.parseDistanceToAddress(placeDirectionsStream);
+        Assertions.assertEquals("0.3 mi", distance);
+    }
+    @Test
+    public  void parseDistanceURL() throws IOException{
+        InputStream directionsPlease = parser.directionsAndDistance("2720 W Jackson St, Muncie, IN 47303, USA", "523 S Tillotson Ave, Muncie, IN 47304, USA");
+        String distance = parser.parseDistanceToAddress(directionsPlease);
         Assertions.assertEquals("0.3 mi", distance);
     }
 }
--- src/test/java/edu/bsu/cs222/model/PlaceNearByTest.java
+++ src/test/java/edu/bsu/cs222/model/PlaceNearByTest.java
@@ -7,12 +7,9 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.io.InputStream;
 
-
 public class PlaceNearByTest {
-
     private final InputStream placeNearByStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("PlaceNearBySearchData.json");
     private final AddressParser parser = new AddressParser();
-
     @Test
     public void parseVenueAddressTest() throws IOException {
         String address = parser.parseVenueAddress(placeNearByStream);
@@ -29,13 +26,33 @@ public class PlaceNearByTest {
         Assertions.assertEquals("true", openValue);
     }
     @Test
-    public void parseVenueLongitudeTest() throws IOException {
-        Double longitude = parser.parseLongitude(placeNearByStream);
-        Assertions.assertEquals(-85.419575,longitude);
+    public void parseVenueLatitudeAndLongitudeTest() throws IOException {
+        Double[] latitudeAndLongitude = parser.parseLatitudeAndLongitude(placeNearByStream);
+        Assertions.assertEquals(40.189823, latitudeAndLongitude[0]);
+        Assertions.assertEquals(-85.419575,latitudeAndLongitude[1]);
+    }
+    //________________U_R_L___T_E_S_T_I_N_G______________
+    @Test
+    public void parseUsersAverageLatitudeAndLongitudeFromURLTest() throws IOException {
+        InputStream userOneAddress = parser.placeFromText("1615 Riverside Avenue, 47303");
+        InputStream userTwoAddress = parser.placeFromText("2800 Tillotson Avenue 47304");
+        Double[] coordinatesA = parser.parseLatitudeAndLongitude(userOneAddress);
+        Double[] coordinatesB = parser.parseLatitudeAndLongitude(userTwoAddress);
+        Double[] coordinates = parser.returnAverage(coordinatesA, coordinatesB);
+
+        Assertions.assertEquals(40.208118150000004, coordinates[0]);
+        Assertions.assertEquals(-85.41264945, coordinates[1]);
     }
     @Test
-    public void parseVenueLatitudeTest() throws IOException {
-        Double latitude = parser.parseLatitude(placeNearByStream);
-        Assertions.assertEquals(40.189823, latitude);
+    public void parseVenueNearByFromURLTest() throws IOException {
+        InputStream place1 = parser.placeFromText("1615 Riverside Avenue 47303, Muncie IN");
+        InputStream place2 = parser.placeFromText("2800 Tillotson Avenue 47304, Muncie IN ");
+        Double[] coordinatesA = parser.parseLatitudeAndLongitude(place1);
+        Double[] coordinatesB = parser.parseLatitudeAndLongitude(place2);
+        Double[] midPointLocation = parser.returnAverage(coordinatesA, coordinatesB);
+
+        InputStream venue = parser.placeNearSearch(midPointLocation[0],midPointLocation[1],"restaurant");
+        String address = parser.parseVenueAddress(venue);
+        Assertions.assertEquals("Ball State University North Dining 201, 1525 North McKinley Avenue, Muncie", address);
     }
 }
