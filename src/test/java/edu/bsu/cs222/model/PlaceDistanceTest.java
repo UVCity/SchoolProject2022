302881de3f9b4e8f5bae1847e8842f3fc273068d
DkirkPorter <steve667a998@gmail.com>
2022-04-13 16:28:59 -0400

    Remove tests that use the API key

--- src/test/java/edu/bsu/cs222/model/PlaceDistanceTest.java
+++ src/test/java/edu/bsu/cs222/model/PlaceDistanceTest.java
@@ -15,10 +15,4 @@ public class PlaceDistanceTest {
         String distance = parser.parseDistanceToAddress(placeDirectionsStream);
         Assertions.assertEquals("0.3 mi", distance);
     }
-    @Test
-    public  void parseDistanceURL() throws IOException{
-        InputStream directionsPlease = parser.directionsAndDistance("2720 W Jackson St, Muncie, IN 47303, USA", "523 S Tillotson Ave, Muncie, IN 47304, USA");
-        String distance = parser.parseDistanceToAddress(directionsPlease);
-        Assertions.assertEquals("0.3 mi", distance);
-    }
 }
