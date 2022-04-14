bfcc2b5b71665f9b0765c8dec0c55dcec0eb2b3b
Dominic <steve667a998@gmail.com>
2022-04-14 10:32:39 -0400

    Create KeyReader and APICaller classes Implement KeyReader and APICaller classes

---
+++ src/main/java/edu/bsu/cs222/APICaller.java
@@ -0,0 +1,22 @@
+package edu.bsu.cs222;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class APICaller {
+    public InputStream tryCatch(String urlString)throws IOException {
+        try {
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+            connection.setRequestProperty("User-Agent",
+                    "Revision Reporter/0.1 (http://www.cs.bsu.edu/~pvg/courses/cs222Sp22; lyle@uv.city)");
+            return connection.getInputStream();
+        }
+        catch (MalformedURLException malformedURLException){
+            throw new RuntimeException(malformedURLException);
+        }
+    }
+}
--- src/main/java/edu/bsu/cs222/URLFormatter.java
+++ src/main/java/edu/bsu/cs222/URLFormatter.java
@@ -1,49 +1,30 @@
 package edu.bsu.cs222;
 
-import com.jayway.jsonpath.JsonPath;
-import net.minidev.json.JSONArray;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
 
 
 public class URLFormatter {
+
+    APICaller APIcaller = new APICaller();
+    KeyReader keyReader = new KeyReader();
+
     public InputStream placeFromText(String address) throws IOException {
         String encodedUrlString = URLEncoder.encode(address, Charset.defaultCharset());
-        String urlString = String.format("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=%s&inputtype=textquery&fields=formatted_address%%2Cgeometry&key=%s",encodedUrlString , key());
-        return tryCatch(urlString);
+        String urlString = String.format("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=%s&inputtype=textquery&fields=formatted_address%%2Cgeometry&key=%s",encodedUrlString , keyReader.key());
+        return APIcaller.tryCatch(urlString);
     }
     public InputStream placeNearSearch(Double latitude, Double longitude, String venueType) throws IOException {
         String formatter = String.format("%f%%2C%f",latitude, longitude);
-        String urlString = String.format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s&rankby=distance&type=%s&formatted_address&key=%s",formatter, venueType, key());
-        return tryCatch(urlString);
+        String urlString = String.format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s&rankby=distance&type=%s&formatted_address&key=%s",formatter, venueType, keyReader.key());
+        return APIcaller.tryCatch(urlString);
     }
     public InputStream directionsAndDistance( String placeFrom , String placeTo) throws IOException {
         String from = URLEncoder.encode(placeFrom, Charset.defaultCharset());
         String to = URLEncoder.encode(placeTo, Charset.defaultCharset());
-        String urlString = String.format("https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&key=%s" ,from, to, key());
-        return tryCatch(urlString);
-    }
-    public InputStream tryCatch(String urlString)throws IOException{
-        try {
-            URL url = new URL(urlString);
-            URLConnection connection = url.openConnection();
-            connection.setRequestProperty("User-Agent",
-                    "Revision Reporter/0.1 (http://www.cs.bsu.edu/~pvg/courses/cs222Sp22; lyle@uv.city)");
-            return connection.getInputStream();
-        }
-        catch (MalformedURLException malformedURLException){
-            throw new RuntimeException(malformedURLException);
-        }
-    }
-    public String key() throws IOException {
-        InputStream Key = Thread.currentThread().getContextClassLoader().getResourceAsStream("./privateResources/Key.Json");
-        JSONArray address = JsonPath.read(Key, "$..key");
-        return address.get(0).toString();
+        String urlString = String.format("https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&key=%s" ,from, to, keyReader.key());
+        return APIcaller.tryCatch(urlString);
     }
 }
