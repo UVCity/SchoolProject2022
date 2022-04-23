ba1d8a35dcf480100743c6343644013cb0d1199a
Dominic <steve667a998@gmail.com>
2022-04-23 14:23:51 -0400

    Change InputStream names

--- src/main/java/edu/bsu/cs222/Model/AddressParser.java
+++ src/main/java/edu/bsu/cs222/Model/AddressParser.java
@@ -8,12 +8,12 @@ import java.io.IOException;
 
 public class AddressParser {
 
-    public String parseVenueAddress(InputStream testDataStream) throws IOException {                                    //nearBySearch Api
-        JSONArray address = JsonPath.read(testDataStream, "$..vicinity");
+    public String parseVenueAddress(InputStream venueUrl) throws IOException {                                    //nearBySearch Api
+        JSONArray address = JsonPath.read(venueUrl, "$..vicinity");
         return address.get(0).toString();
     }
-    public String parseName(InputStream testDataStream) throws IOException {                                           //nearBySearch Api
-        JSONArray name = JsonPath.read(testDataStream, "$..name");
+    public String parseName(InputStream venueUrl) throws IOException {                                           //nearBySearch Api
+        JSONArray name = JsonPath.read(venueUrl, "$..name");
         return name.get(0).toString();
     }
     public String parseHoursOfOperation(InputStream venueUrl) throws IOException {                               //nearBySearch Api
