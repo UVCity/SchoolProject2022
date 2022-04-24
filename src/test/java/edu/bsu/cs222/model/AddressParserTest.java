53968ce161b6913645742e64b5f04d7265530013
Dominic <steve667a998@gmail.com>
2022-04-24 14:13:38 -0400

    Integrate methods in AddressParserTest

--- src/test/java/edu/bsu/cs222/model/AddressParserTest.java
+++ src/test/java/edu/bsu/cs222/model/AddressParserTest.java
@@ -1,8 +1,6 @@
 package edu.bsu.cs222.model;
 
 import edu.bsu.cs222.Model.AddressParser;
-import net.minidev.json.JSONObject;
-import net.minidev.json.parser.JSONParser;
 import net.minidev.json.parser.ParseException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,20 +12,28 @@ public class AddressParserTest {
     private final InputStream placeNearByStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("PlaceNearBySearchData.json");
     private final AddressParser parser = new AddressParser();
 
+    {
+        try {
+            parser.createJsonObject(placeNearByStream);
+        } catch (IOException | ParseException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 
     @Test
     public void parseVenueAddressTest() throws IOException {
-        String address = parser.parseVenueAddress(placeNearByStream);
+        String address = parser.parseVenueAddress();
         Assertions.assertEquals("523 South Tillotson Avenue, Muncie",address);
     }
     @Test
     public void parseVenueHoursOfOperationTest() throws IOException {
-        String openValue = parser.parseHoursOfOperation(placeNearByStream);
+        String openValue = parser.parseHoursOfOperation();
         Assertions.assertEquals("true", openValue);
     }
     @Test
     public void parseVenueNameTest() throws IOException {
-        String name = parser.parseName(placeNearByStream);
+        String name = parser.parseName();
         Assertions.assertEquals("Starbucks",name);
     }
 }
